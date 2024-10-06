2f79098b68f139daafc8ce832462f1b339b5848c
hanna.khokhlova <hanna.khokhlova@ringteam.com>
2024-10-06 14:14:31 +0300

    add solution for Java OOP Part I homework

--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,30 @@
 public class Student {
     int rating;
     private String name;
+    private static int studentCount = 0;
+    private static int sumOfRatings = 0;
 
     // TODO implement Student class according to the instructions provided in the README.md file
 
+    public Student() { studentCount++; }
+
     public Student(String name) {
-        //TODO initialize name
+        this.name = name;
+        studentCount++;
+    }
+
+    public Student(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+        studentCount++;
+        sumOfRatings += this.rating;
     }
 
     public static double getAvgRating() {
         // TODO return average rating of all students
+        if (studentCount > 0) {
+            return (double) sumOfRatings / studentCount;
+        }
         return 0;
     }
 
@@ -18,7 +33,7 @@ public class Student {
     }
 
     public void setName(String name) {
-        // TODO set student's name
+        this.name = name;
     }
 
     public int getRating() {
@@ -26,25 +41,43 @@ public class Student {
     }
 
     public void setRating(int rating) {
-        // TODO initialize rating;
+        this.rating = rating;
+        sumOfRatings += this.rating;
     }
 
     public boolean betterStudent(Student student) {
         // TODO return the result of comparing this.student's rating with the student's rating
-        return false;
+        return this.rating > student.rating;
     }
 
     public void changeRating(int rating) {
         // TODO change this student's rating and average rating of all students
+        sumOfRatings = sumOfRatings - this.rating + rating;
+        this.rating = rating;
     }
 
     public static void removeStudent(Student student) {
-        // TODO remove student
+        Student.studentCount--;
+        Student.sumOfRatings -= student.rating;
     }
 
     @Override
     public String toString() {
         // TODO return String with name and rating of this student
-        return "";
+        return "Stubent's name is " + name + ", rating is " + rating;
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setName("Anna");
+        student1.setRating(5);
+        Student student2 = new Student("Andrii");
+        student2.setRating(4);
+        Student student3 = new Student("Olga",  3);
+        System.out.println(sumOfRatings);
+        System.out.println(studentCount);
+        System.out.println(Student.getAvgRating());
+        student3.changeRating(5);
+        System.out.println("New avg rating is " + Student.getAvgRating());
     }
 }
