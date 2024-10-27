6d694dca5bdc6381c685ea3432eec70114d11658
hanna.khokhlova <hanna.khokhlova@ringteam.com>
2024-10-27 16:53:55 +0200

    Java Data Types homework

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,9 @@ public class TemperatureTest {
         return new Object[][]{
                 {212.0, 100.0},
                 {32.0, 0.0},
-                {-15, -26.11111111111111}
-                // TODO add 2 more test data here
+                {-15, -26.11111111111111},
+                {41, 5},
+                {13.1, -10.5}
         };
     }
 
@@ -28,8 +29,9 @@ public class TemperatureTest {
         return new Object[][]{
                 {100.0, 212.0},
                 {0.0, 32.0},
-                {-26.11111111111111, -15}
-                // TODO add 2 more test data here
+                {-26.11111111111111, -15},
+                {5, 41},
+                {-10.5, 13.1}
         };
     }
 
