eb8ccf2e037b87eebd0d2c8ee062a3f556bdb138
hanna.khokhlova <hanna.khokhlova@ringteam.com>
2024-11-13 17:53:53 +0200

    Basics lambda homework

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -15,8 +15,9 @@ public class LambdaTest {
                 {-1.0, 10.0, 10.0},
                 {0.0, 0.0, 0.0},
                 {-10.0, NaN, NaN},
-                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY},
+                {3.0, 2.5, 3.0},
+                {-1.5, -0.8, -0.8}
         };
     }
 
@@ -25,8 +26,9 @@ public class LambdaTest {
         return new Object[][]{
                 {25.0, 5.0},
                 {-1.0, NaN},
-                {POSITIVE_INFINITY, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+                {POSITIVE_INFINITY, POSITIVE_INFINITY},
+                {4.0, 2.0},
+                {9.0, 3.0},
         };
     }
 
