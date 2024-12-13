03dd3c1361e9dec1f41967a66f70d5fed62a6950
hanna.khokhlova <hanna.khokhlova@ringteam.com>
2024-12-13 09:39:16 +0200

    Selenium homework

--- src/test/java/com/example/selenium/BaseTest.java
+++ src/test/java/com/example/selenium/BaseTest.java
@@ -1,7 +1,10 @@
 package com.example.selenium;
 
 import com.example.selenium.steps.UserSteps;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -12,10 +15,20 @@ public class BaseTest {
     @BeforeClass
     public void setUp() {
         // TODO initialize a driver, open login URL, resize windows, and initialize steps
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.manage().window().setSize(new Dimension(840, 1027));
+        userSteps = new UserSteps(driver);
+
     }
 
     @AfterClass
     public void tearDown() {
         // TODO driver close and quit
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
     }
 }
